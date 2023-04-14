8cd1c631e663d058cd7e4bc6decc158953eac0f2
Wenderson Pires <wendersonpdas@gmail.com>
2023-04-14 01:35:42 -0300

    default code message fixed

--- constants.js
+++ constants.js
@@ -2,18 +2,7 @@ const defaultCodeMessage = `
 return (
   <div>
     <p style={{ fontWeight: 600, color: "#AB2E28", fontFamily: "Courier new" }}>
-      The Widget file was found. You must run the following command
-      to inject your widget:
-      <span
-        style={{
-          background: "#000",
-          color: "#fff",
-          padding: "2px",
-          fontWeight: 400,
-        }}
-      >
-        npx init-viewer path/to/MyWidget.jsx
-      </span>
+      No Widget file found!
     </p>
   </div>
 );
